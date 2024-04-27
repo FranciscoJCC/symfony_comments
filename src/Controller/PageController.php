766aac1637f7f935a81ecd8acd3ae1839c046b69
Francisco Cervantes <fjccandelario@gmail.com>
2024-04-27 02:52:33 -0600

    Se agrega formulario para agregar comentarios

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,24 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController {
 
-    #[Route('/')]
-    public function home(EntityManagerInterface $entityManager): Response {
+    #[Route('/', name: 'home')]
+    public function home(EntityManagerInterface $entityManager, Request $request): Response {
         
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
         $comments = $entityManager->getRepository(Comment::class)->findBy([], [
             'id' => 'DESC'
         ]);
 
         /* return new Response('Welcome, página home ' . $search); */
         return $this->render('home.html.twig', [
-            'comments' => $comments
+            'comments' => $comments,
+            'form' => $form->createView()
         ]);
     }
 }
