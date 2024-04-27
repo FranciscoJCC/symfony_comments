d45a309fe9ebbb5754949d4845d4442118ea32d6
Francisco Cervantes <fjccandelario@gmail.com>
2024-04-27 02:36:08 -0600

    Se muestran comentarios en el page controller

--- migrations/Version20240427080431.php
+++ migrations/Version20240427080431.php
@@ -20,12 +20,12 @@ final class Version20240427080431 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, body VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, body VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE comments');
+        $this->addSql('DROP TABLE comment');
     }
 }
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController {
 
     #[Route('/')]
-    public function home(Request $request) {
+    public function home(EntityManagerInterface $entityManager): Response {
         
-        $search = $request->get('search');
-        
+        $comments = $entityManager->getRepository(Comment::class)->findBy([], [
+            'id' => 'DESC'
+        ]);
+
         /* return new Response('Welcome, página home ' . $search); */
-        return $this->render('home.html.twig', ['search' => $search]);
+        return $this->render('home.html.twig', [
+            'comments' => $comments
+        ]);
     }
 }
